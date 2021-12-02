03a8f6a23813bb192b2f894964a75c169f478f80
dylongaddi <dylongaddi@gmail.com>
2021-12-01 18:56:47 -0800

    Added simple UI for rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,50 +1,38 @@
-function computerPlay() {
-        let randomChoice = Math.floor(Math.random()*3);
-        if (randomChoice === 1) {
-                return 'rock';
-        }   else if (randomChoice === 2) {
-                return 'paper';
-        }   else {
-                return 'scissors';
-        }
-    }
-
-
 function playRound(playerSelection, computerSelection) {
-                if (playerSelection === 'ROCK') {
-                        if (computerSelection === 'paper') {
+                if (playerSelection === 'Rock') {
+                        if (computerSelection === 'Paper') {
                                 computerScore++;
-                                return computerWin;
-                        }   else if (computerSelection === 'rock') {
-                                return draw;
-                        }   else if (computerSelection === 'scissors') {
+                                roundResult = computerWin;
+                        }   else if (computerSelection === 'Rock') {
+                                roundResult = draw;
+                        }   else if (computerSelection === 'Scissors') {
                                 playerScore++;
-                                return playerWin;
+                                roundResult = playerWin;
                         } else {
                                 return 'Something Went Wrong';
                         }
                 } 
-                else if (playerSelection.toUpperCase === 'PAPER') {
-                        if (computerSelection === 'rock') {
+                else if (playerSelection === 'Paper') {
+                        if (computerSelection === 'Rock') {
                                 playerScore++;
-                                return playerWin;
-                        }   else if (computerSelection === 'paper') {
-                                return draw;
-                        }   else if (computerSelection === 'scissors') {
+                                roundResult = playerWin;
+                        }   else if (computerSelection === 'Paper') {
+                                roundResult = draw;
+                        }   else if (computerSelection === 'Scissors') {
                                 computerScore++;
-                                return computerWin;
+                                roundResult = computerWin;
                         } else {
                         return 'Something Went Wrong';
                 }
                 }
-                else if (playerSelection.toUpperCase === 'SCISSORS') {
-                        if(computerSelection === 'rock') {
+                else if (playerSelection === 'Scissors') {
+                        if(computerSelection === 'Rock') {
                         computerScore++;
                         return computerWin; 
-                }   else if (computerSelection === 'paper') {
+                }   else if (computerSelection === 'Paper') {
                         playerScore++;
                         return playerWin;
-                }   else if (computerSelection === 'scissors') {
+                }   else if (computerSelection === 'Scissors') {
                         return draw;
                 }   else {
                         return 'Something went wrong';
@@ -55,22 +43,62 @@ function playRound(playerSelection, computerSelection) {
 let playerWin = 'You win!'
 let computerWin = 'The computer wins!'
 let draw= 'It is a draw!'
+let roundResult = ''
 let computerScore = 0
 let playerScore = 0
 
+const selections = document.querySelectorAll('.selections');
+selections.forEach((selection) => {
+        selection.addEventListener('click', function () {
+        const playerSelection= this.textContent;
 
-for (var i=0;i<5;i++) {
-        let playerSelection = prompt('Choose Rock, Paper, or Scissors').toUpperCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log('Player:' + playerScore);
-        console.log('Computer:' + computerScore);
+        const computerOptions = ["Rock", "Paper", "Scissors"]
+        const computerSelection = computerOptions[Math.floor(Math.random()*3)];
+
+        playRound(playerSelection, computerSelection);
+        updateScore();
+        checkWinner();
+        });
+});
+
+
+
+function updateScore() {
+        document.getElementById('pScore').textContent = playerScore;
+        document.getElementById('cScore').textContent = computerScore;
+        document.getElementById('roundResult').textContent = roundResult;
 }
 
-if (playerScore > computerScore) {
-                console.log('Player Wins!');
-        } else if (playerScore === computerScore) {
-                console.log('It is a tie!');
-        } else {
-                console.log('Computer Wins!');
+
+
+//UI below is a WIP
+
+function openWinnerModal () {
+
+}
+
+function closeWinnerModal () {
+
+}
+
+function setModalMessage () {
+        return playerScore > computerScore
+        ? (winnerMessage.textContent = 'You win the series this time!')
+        : (winnerMessage.textContent = 'The computer wins this time!')
+}
+
+function restartGame () {
+        playerScore = 0
+        computerScore = 0
+        document.getElementById('roundResult').textContent = 'Choose your weapon!'
+}
+
+function checkWinner () {
+        
+        if (playerScore === 5 || computerScore === 5) {
+                restartGame();
+                return playerScore > computerScore
+                ? (document.getElementById('roundResult').textContent = "You win the series this time!") 
+                : (document.getElementById('roundResult').textContent = "The computer wins the series this time!")
         }
+}
